8d57ea813ca693945a14c63448f6bba7412c2253
Thomas Junk <trj@fnal.gov>
2025-01-30 14:01:17 -0600

    remove unused variables to make clang happy

--- Geometry/ChannelMapAlgs/ChannelMapStandardAlg_toad.h
+++ Geometry/ChannelMapAlgs/ChannelMapStandardAlg_toad.h
@@ -100,7 +100,7 @@ namespace gar{
                 std::vector<UInt_t> fCenterFirstPadInRow;        ///< first pad in row for center hole filler
 
                 float               fCenterPadWidth;             ///< Width of square pads in center hole filler
-                UInt_t              fNumChansCenter;             ///< Number of channels in center hole filler
+	      // commented to make clang happy UInt_t              fNumChansCenter;             ///< Number of channels in center hole filler
 
                 void                CheckPositions();            ///< Method to check consistency of NearestChannel and ChannelToPosition
 
